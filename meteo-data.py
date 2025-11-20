b9ff4df84cce70e3956bfce49d44a1591c9b9a4d
swave <ersin.kilic@student.hu.nl>
2025-11-20 14:26:18 +0100

    meteo API data

---
+++ meteo-data.py
@@ -0,0 +1,6 @@
+import requests
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relative_humidity_2m,rain,snowfall,cloud_cover,pressure_msl,wind_speed_10m,weather_code&timezone=Europe%2FBerlin"
+response = requests.get(url)
+response.raise_for_status()
+data = response.json()
